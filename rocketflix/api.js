c8610aa7bb8267e1e9fe459fbfd8cec0cd8d3d5a
luizgfalqueto <lgfalqueto85@gmail.com>
2022-03-13 11:57:51 -0300

    configure API

--- rocketflix/api.js
+++ rocketflix/api.js
@@ -1,4 +1,6 @@
-export const API_KEY = 'api_key=CHAVE_API';
-export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const API_KEY = 'api_key=CHAVE_API';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const CONSULT = '/discover/movie?sort_by=popularity.desc&'
+export const API_URL = BASE_URL + CONSULT + API_KEY
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 export const language = 'language=pt-BR';
