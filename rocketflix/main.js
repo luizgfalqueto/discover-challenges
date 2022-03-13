2cfb502b1622783cd598fea60f81ee74be5b0cf2
luizgfalqueto <lgfalqueto85@gmail.com>
2022-03-13 11:59:47 -0300

    using API TMDB

--- rocketflix/main.js
+++ rocketflix/main.js
@@ -1,5 +1,38 @@
 import {
-  API_KEY, BASE_URL,
+  API_URL,
   IMG_URL,
   language,
 } from './api.js'
+
+document.getElementById('generator').addEventListener('click', () => {
+  getMovie(API_URL)
+})
+
+function getMovie(url) {
+  fetch(url).then(res => res.json()).then(data => {
+    showMovie(data.results)
+  })
+}
+
+const poster = document.querySelector('.poster')
+const details = document.querySelector('.details')
+
+
+function showMovie(data) {
+
+
+  poster.innerHTML = ''
+  details.innerHTML = ''
+
+  const ramdon = Math.floor(Math.random() * (19 - 0 + 1) + 0);
+  
+  const title = data[ramdon].title
+  const overview = data[ramdon].overview
+  const movie_poster = IMG_URL + data[ramdon].poster_path
+
+  poster.innerHTML = `<img src="${movie_poster}" alt="${title}">`
+  details.innerHTML = `
+  <h2>${title}</h2>
+  <p>${overview}</p>
+  `
+}
